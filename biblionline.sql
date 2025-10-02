babff8400782273514606e0420d4294ff3135991
Linuxxzzz <lqao230528@upemor.edu.mx>
2025-10-02 02:46:54 -0600

    Triggers para los registros en la tabla historial de la BD

--- biblionline.sql
+++ biblionline.sql
@@ -40,8 +40,45 @@ CREATE TABLE Alumno (
     contrasena VARCHAR(100)
 );
 
--- Tabla: Historial
+-- Tabla: Historial (Bitácora)
 CREATE TABLE Historial (
     id_historial INT PRIMARY KEY auto_increment,
     mensaje varchar(1000)
 );
+
+/* ================================================
+   TRIGGERS PARA BITÁCORA
+   ================================================ */
+
+-- Trigger para tabla Administrador
+DROP TRIGGER IF EXISTS registroBitacoraAdministrador;
+DELIMITER //
+CREATE TRIGGER registroBitacoraAdministrador AFTER INSERT ON Administrador FOR EACH ROW
+BEGIN
+    INSERT INTO Historial 
+    VALUES (0, CONCAT_WS(' ', 'Administrador Registrado:', NEW.nombre, NEW.apellidos, 'Correo:', NEW.correo, 'Cargo:', NEW.cargo, 'Usuario:', USER(), 'Fecha:', NOW()));
+END;
+//
+DELIMITER ;
+
+-- Trigger para tabla Docente
+DROP TRIGGER IF EXISTS registroBitacoraDocente;
+DELIMITER //
+CREATE TRIGGER registroBitacoraDocente AFTER INSERT ON Docente FOR EACH ROW
+BEGIN
+    INSERT INTO Historial 
+    VALUES (0, CONCAT_WS(' ', 'Docente Registrado:', NEW.nombre, NEW.apellidos, 'Correo:', NEW.correo, 'Especialidad:', NEW.especialidad, 'Usuario:', USER(), 'Fecha:', NOW()));
+END;
+//
+DELIMITER ;
+
+-- Trigger para tabla Alumno
+DROP TRIGGER IF EXISTS registroBitacoraAlumno;
+DELIMITER //
+CREATE TRIGGER registroBitacoraAlumno AFTER INSERT ON Alumno FOR EACH ROW
+BEGIN
+    INSERT INTO Historial 
+    VALUES (0, CONCAT_WS(' ', 'Alumno Registrado:', NEW.nombre, NEW.apellidos, 'Matrícula:', NEW.matricula, 'Correo:', NEW.correo, 'Usuario:', USER(), 'Fecha:', NOW()));
+END;
+//
+DELIMITER ;
